88ef272d2459d5e27cee5b2b375c5be846d5aa80
soubj <sobujahmed124@gmail.com>
2023-07-27 16:17:00 +0600

    creating jwt token and sending it to client

---
+++ README.md
@@ -0,0 +1,4 @@
+## Generate secret key using node in terminal
+
+- write [node] in terminal
+- write [require('crypto').randomBytes(64).toString('hex')] to generate 64 bytes hex code
--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
+const jwt = require("jsonwebtoken");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -28,6 +30,17 @@ async function run() {
     const serviceCollection = client.db("carDoctor").collection("services");
     const bookingCollection = client.db("carDoctor").collection("bookings");
 
+    // jwt
+    app.post("/jwt", (req, res) => {
+      const user = req.body;
+      console.log(user);
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "1h",
+      });
+      res.send({ token });
+    });
+
+    // get services from database
     app.get("/services", async (req, res) => {
       const cursor = serviceCollection.find();
       const result = await cursor.toArray();
